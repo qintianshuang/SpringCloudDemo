73652e6fe42e463d788d200a39da9ccb65b50362
qintianshuang <asdfg716622>
2019-07-20 20:30:56 +0800

    更新代码

--- cloud-service-util/src/main/java/com/example/cloud/service/util/RedisUtil.java
+++ cloud-service-util/src/main/java/com/example/cloud/service/util/RedisUtil.java
@@ -3,13 +3,14 @@ package com.example.cloud.service.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
-//import org.apache.log4j.Logger;
 import com.example.cloud.service.config.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.ShardedJedisPool;
+
+//import org.apache.log4j.Logger;
 
 @Component("RedisUtil")
 public class RedisUtil {
